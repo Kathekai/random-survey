33677151d9d761a8e87b3a4999a939c45e1956a6
Kathekai <kaikai8393@gmail.com>
2024-10-31 12:29:04 +0800

    Update url.js

--- url.js
+++ url.js
@@ -10,7 +10,6 @@
 const urls = [
 	'https://www.surveycake.com/s/egYYz',
 	'https://www.surveycake.com/s/NqZXM',
-	'https://www.surveycake.com/s/Yg7RZ',
 	'https://www.surveycake.com/s/AxZAa',
 	'https://www.surveycake.com/s/2Xm2l',
 	'https://www.surveycake.com/s/zrLV2',
